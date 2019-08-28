918566d07950d4dac328428099dfb30c14586483
Fangting Zhou <fangting@stat.tamu.edu>
2019-08-27 20:29:02 -0500

    Write a function to generate Y

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -5,6 +5,9 @@
 # seed  - starting seed value
 generateY <- function(X, beta, sigma, seed = 5832652){
   #[ToDo] Set seed and generate Y following linear model
+  set.seed(seed)
+  
+  Y = X %*% beta + rnorm(nrow(X), 0, sigma)
   
   # Return Y
   return(Y)
